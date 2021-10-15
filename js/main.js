e48bc82478e1594fd65a19288ae257cca7c632fc
mlmirk <mike.l.mirkovic@gmail.com>
2021-10-15 16:43:42 -0400

    parse clicks into category and which questionnwill be selected

--- js/main.js
+++ js/main.js
@@ -10,19 +10,22 @@ let playerTurn
 let winner
 let player1Score
 let player2Score
+let catagoryNum
+let questionNum
 
 
 /*------------------------ Cached Element References ------------------------*/
 const cells = document.querySelectorAll(".cell")
 const playerMessage= document.querySelector("#message")
+const player1ScoreMessage=document.querySelector("#player1")
+const player2ScoreMessage=document.querySelector("#player2")
 
-console.log(playerMessage)
+
+//console.log(player2ScoreMessage)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-cells.forEach(cell=> cell.addEventListener('click',()=>{
-  console.log(cell)
-}))
+cells.forEach(cell=> cell.addEventListener('click',handleClick))
 
 
 
@@ -31,3 +34,36 @@ cells.forEach(cell=> cell.addEventListener('click',()=>{
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init(){
+playerTurn = 1
+player1Score=0
+player2Score=0
+winner= null
+
+render()
+}
+
+function render(){
+playerMessage.innerHTML= playerTurn >0 ? "It is player 1's turn":"It is player 2's turn"
+
+
+
+}
+
+function handleClick(evt){
+playerTurn= playerTurn * -1
+console.log(evt.target.id)
+temp=evt.target.id
+catagoryNum= parseInt(temp.slice(0))
+questionNum= parseInt(temp.slice(-1))
+console.log(questionNum)
+// cells.setAttribute('hidden')
+
+
+render()
+}
+
+
+init()
+
