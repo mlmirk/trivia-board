81f3ad6fef71d9823cdf5be3d9e8196a20399dd8
mlmirk <mike.l.mirkovic@gmail.com>
2021-10-15 15:31:35 -0400

    v1 data structure for questions

---
+++ js/questions.js
@@ -0,0 +1,324 @@
+const Question0=[
+  { question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+
+  },
+  {question:'',
+  rightAnswer:'',
+  wrongAnswer1:'',
+  wrongAnswer2:'',
+  wrongAnswer3:'',
+  isActive:true, 
+
+
+  },
+  {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+
+
+  },
+  {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+
+
+  },
+  {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+
+
+  },
+  {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+
+  }]
+
+
+  const Question1=[
+    { question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    },
+    {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    }]
+
+  const Question2=[
+    { question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    },
+    {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    }]
+
+  const Question3=[
+    { question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    },
+    {question:'',
+    rightAnswer:'',
+    wrongAnswer1:'',
+    wrongAnswer2:'',
+    wrongAnswer3:'',
+    isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+  
+    },
+    {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+  
+    }]
+
+    const Question4=[
+      { question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+    
+      },
+      {question:'',
+      rightAnswer:'',
+      wrongAnswer1:'',
+      wrongAnswer2:'',
+      wrongAnswer3:'',
+      isActive:true, 
+    
+    
+      },
+      {question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+    
+    
+      },
+      {question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+    
+    
+      },
+      {question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+    
+    
+      },
+      {question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+    
+      }]
+      const Question5=[
+        { question:'',
+          rightAnswer:'',
+          wrongAnswer1:'',
+          wrongAnswer2:'',
+          wrongAnswer3:'',
+          isActive:true, 
+      
+        },
+        {question:'',
+        rightAnswer:'',
+        wrongAnswer1:'',
+        wrongAnswer2:'',
+        wrongAnswer3:'',
+        isActive:true, 
+      
+      
+        },
+        {question:'',
+          rightAnswer:'',
+          wrongAnswer1:'',
+          wrongAnswer2:'',
+          wrongAnswer3:'',
+          isActive:true, 
+      
+      
+        },
+        {question:'',
+          rightAnswer:'',
+          wrongAnswer1:'',
+          wrongAnswer2:'',
+          wrongAnswer3:'',
+          isActive:true, 
+      
+      
+        },
+        {question:'',
+          rightAnswer:'',
+          wrongAnswer1:'',
+          wrongAnswer2:'',
+          wrongAnswer3:'',
+          isActive:true, 
+      
+      
+        },
+        {question:'',
+          rightAnswer:'',
+          wrongAnswer1:'',
+          wrongAnswer2:'',
+          wrongAnswer3:'',
+          isActive:true, 
+      
+        }]
+      
