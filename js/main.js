228184ff4d9602ad39049dae667aa48b1b5fd419
mlmirk <mike.l.mirkovic@gmail.com>
2021-10-15 15:39:03 -0400

    event listener mapped to each cell

--- js/main.js
+++ js/main.js
@@ -6,18 +6,23 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+let playerTurn
+let winner
+let player1Score
+let player2Score
 
 
+/*------------------------ Cached Element References ------------------------*/
+const cells = document.querySelectorAll(".cell")
+const playerMessage= document.querySelector("#message")
 
-
-
-
-
-
+console.log(playerMessage)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+cells.forEach(cell=> cell.addEventListener('click',()=>{
+  console.log(cell)
+}))
 
 
 
