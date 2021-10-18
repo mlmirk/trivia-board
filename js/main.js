3094a0337d867bde687bedb2e4fffd36dd07a1da
mlmirk <mike.l.mirkovic@gmail.com>
2021-10-18 16:07:13 -0400

    export/import function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 import{getQuestionsAndAnswers} from "./questions.js"
 
-
-
-
-
 /*---------------------------- Variables (state) ----------------------------*/
 let playerTurn
 let winner
@@ -15,6 +11,8 @@ let questionNum
 let questionCorrect
 let questionActive
 let timeLeft
+let questionObject
+let questionDisplay
 /*------------------------ Cached Element References ------------------------*/
 const cells = document.querySelectorAll(".cell")
 const playerMessage= document.querySelector("#message")
@@ -22,14 +20,11 @@ const player1ScoreMessage=document.querySelector("#player1")
 const player2ScoreMessage=document.querySelector("#player2")
 const questionModal= document.getElementById("questionsModal")
 
-//console.log(questionModal)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 cells.forEach(cell=> cell.addEventListener('click',handleClick))
 questionModal.addEventListener('show.bs.modal',modalEditor)
-
-
 
 
 
@@ -43,6 +38,8 @@ player2Score=0
 winner= null
 questionActive= false
 timeLeft=30
+questionObject={}
+
 render()
 }
 
@@ -50,22 +47,25 @@ function render(){
 playerMessage.innerHTML= playerTurn >0 ? "It is player 1's turn":"It is player 2's turn"
 player1ScoreMessage.innerHTML= `Player 1 Score: ${player1Score}`
 player2ScoreMessage.innerHTML= `Player 2 Score: ${player2Score}`
-console.log(getQuestionsAndAnswers(0,0))
+
 }
 
 function handleClick(evt){
+
 playerTurn= playerTurn * -1
 console.log(evt.target.id)
-temp=evt.target.id
-catagoryNum= parseInt(temp.slice(0))
-questionNum= parseInt(temp.slice(-1))
-console.log(questionNum)
+catagoryNum= parseInt(evt.target.id.slice(0))
+questionNum= parseInt(evt.target.id.slice(-1))
+questionObject=getQuestionsAndAnswers(catagoryNum,questionNum)
+//console.log(questionNum)
 // cells.setAttribute('hidden')
 evt.target.style.visibility = 'hidden'
+//questionObject=getQuestionsAndAnswers(catagoryNum,questionNum)
 questionTimer()
+
+
 render()
 }
-
 function questionTimer(){
 //thirty second timer 
 
@@ -81,10 +81,6 @@ timeLeft=30
 }
 
 function modalEditor(evt){
-// let clicked = evt.relatedTarget
-//   console.log(clicked);
-// i will need to change the value for radio butons this is for when things are selected
-  
 let modalTitle= questionModal.querySelector('.modal-title')
 let selectQuestion= questionModal.querySelector('#question-text')
 let answer1= questionModal.querySelector('label[for="A1"]')
@@ -92,17 +88,26 @@ let answer2= questionModal.querySelector('label[for="A2"]')
 let answer3= questionModal.querySelector('label[for="A3"]')
 let answer4= questionModal.querySelector('label[for="A4"]')
 let timerQ= questionModal.querySelector('#timer')
+
+timerQ.textContent= `time remaining ${timeLeft}` // not truly showing countdown 
+selectQuestion.textContent= `${questionObject.question}`
+
+
+
+// let clicked = evt.relatedTarget
+//   console.log(clicked);
+// i will need to change the value for radio butons this is for when things are selected
 // modalTitle.textContent= 'this Worksss?!' // will change later
 // selectQuestion.textContent='he is the question' // just testing functionality
 // answer1.textContent='123'
 // answer2.textContent='456'
 // answer3.textContent='798'
 // answer4.textContent='564'
-timerQ.textContent= `time remaining ${timeLeft}` // not truly showing countdown 
+}
+function objectHandler(){
 
 
 }
-
 
 
 
