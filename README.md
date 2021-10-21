21fd55b8c6adb1e6d2373309586d6f777ffa1257
mlmirk <mike.l.mirkovic@gmail.com>
2021-10-21 15:25:08 -0400

    Question data structure finished

--- README.md
+++ README.md
@@ -1,26 +1,41 @@
 # Trivia Duos 
 ## __Game overview__
-This browser ennabled trivia game was created with the intention to be a fun, and easy to you head to head trivia game. 
+---
+This browser enabled trivia game was created with the intention to be a fun, and easy to you head to head trivia game. 
 
 
 ## __Starting the Game__
-Face off in a round of trivia by clicking **[here](https://vigorous-khorana-7e3a41.netlify.app)**
+---
+Face off in a round of trivia by clicking **[here](https://vigorous-khorana-7e3a41.netlify.app)**!
 Simply click a square and the game will start. You have a certain amount of time before the game will autosubmit your answer and then award points if you are right!
 
-### Faq & Rules 
-1. Boxes are color coded for difficulty and are equal to diffrent point totals which are: 
- *  Green is easy and worth 100 points
- *  Yellow is medium difficulty and worth 300 points
- *  Red are the hardest questions worth 500 points
-2. The rows are individaul catagories though that infomation is hidden Beware of the unknown
+## FAQ & Rules
+--- 
+1. Boxes are color coded for difficulty and are equal to different point totals which are: 
+   *  Green is easy and worth 100 points
+   *  Yellow is medium difficulty and worth 300 points
+   *  Red are the hardest questions worth 500 points
+2. The rows are individual categories though that information is hidden Beware of the unknown
 3. There is a time limit you have to guess after that time passes the popup will close and whatever answer is selected will be automatically submitted
 4. Winner will be determined by points after the final turn or when the END GAME is clicked. 
-5. Replay will reset the board to origianl condition 
-6. End game will check who won and clear the baord.
-7. Light an dark mode will switch the mode for users based on preferance. 
+5. Replay will reset the board to original condition 
+6. End game will check who won and clear the board.
+7. Light and dark mode will switch the mode for users based on preference. 
+
+## Technologies Used:
+ * HTML
+ * CSS
+ * JavaScript
+ * Bootstrap
 
 
-# link to game
-# tech used/credits
 # screenshots
-# futre stuff
+
+
+
+## Future Enhancements/ Next Steps
+---
+* Greater pool of questions and categories
+* More thoughtful design of modals
+* A win game animation/sound/message
+  
--- js/questions.js
+++ js/questions.js
@@ -98,98 +98,98 @@ const Food0=[
       ]
 
     const Avatar3=[
-      { question:'What is the main ingredient used to make hummus?',
-        rightAnswer:'Chickpeas',
-        answers:["Tahini","Humms","Chickpeas","Navy Beans"],
+      { question:'What is the Order of the Avatar Cycle?(Avatar the Last Airbender)',
+        rightAnswer:'Fire, Air, Water, Earth',
+        answers:["Fire, Air, Water, Earth","Water, Air, Earth, Fire","Earth, Air, Water, Fire","Fire, Water, Earth, Air"],
     }
     ,
-    { question:'Which famous soft drink was invented in 1892?',
-        rightAnswer:'Coca Cola',
-        answers:["Coca Cola","Pepsi","Surge","Fanta"],
+    { question:'Who Started the Tradition of Hunting Dragons for Glory?',
+        rightAnswer:'Fire Lord Sozin',
+        answers:["Fire Lord Azulon","Fire Lord Sozin","Fire Lord Ozai","Fire Lord Iroh"],
     }
     ,
-    { question:'Which is the only food that can never go off or go bad?',
-        rightAnswer:'Honey',
-        answers:["Honey","Twinkies","Peanuts","Canned Tuna"],
+    { question:'Finish the tune: "Two lovers, forbidden from one another..."',
+        rightAnswer:'...war divides their people',
+        answers:["...war divides their people","...secret tunnel, secret tunnel","...through the mountains they go","destined to be together"],
     }
     ,
-    { question:'What popular Mexican dish literally translates as ‘little donkey’?',
-        rightAnswer:'Burrito',
-        answers:["Burrito","Enchilladas","Frijoles","Taco"],
+    { question:'What is the name of Rokus earthbending master and how does Roku describe him?',
+        rightAnswer:'Sud; Uncompromising, stubborn, and blunt and a lifelong friend.',
+        answers:["Sud; Uncompromising, stubborn, and blunt and a lifelong friend.","Ren; Uncompromising, stubborn, and blunt and a lifelong friend.","Wei; Determined, robust, and blunt and a lifelong friend.","Sud; Determined, robust, and blunt and a lifelong friend."],
     }
     ,
-    { question:'How many herbs and spices make up the KFC spice blend?',
-        rightAnswer:'11',
-        answers:["7","9","11","15"],
+    { question:'What is the name of the sinister spirit also known as "the face stealer?"',
+        rightAnswer:'Koh',
+        answers:["Koh","La","Wa Shi Tong","Tui"],
     }
     ,
-    { question:'What is Japanese Sake made from?',
-        rightAnswer:'Rice',
-        answers:["Rice","Barley","Wheat","Wasabi"],
+    { question:'How many years was Aang trapped in the iceberg ?',
+        rightAnswer:'100',
+        answers:["100","1","50","200"],
     }
     
       ]
       const Naruto4=[
-        { question:'What is the main ingredient used to make hummus?',
-          rightAnswer:'Chickpeas',
-          answers:["Tahini","Humms","Chickpeas","Navy Beans"],
+        { question:'Name the character that can only use Taijutsu?',
+          rightAnswer:'Rock Lee',
+          answers:["Sakura","Ten Ten","Rock Lee","Sia"],
       }
       ,
-      { question:'Which famous soft drink was invented in 1892?',
-          rightAnswer:'Coca Cola',
-          answers:["Coca Cola","Pepsi","Surge","Fanta"],
+      { question:'Name the First Hokage',
+          rightAnswer:'Hashirama Senju',
+          answers:["Hashirama Senju","Tobyrama Senju","Madara Uchiha","Gaara"],
       }
       ,
-      { question:'Which is the only food that can never go off or go bad?',
-          rightAnswer:'Honey',
-          answers:["Honey","Twinkies","Peanuts","Canned Tuna"],
+      { question:'Whom did Kakashi get his eye from?',
+          rightAnswer:'Obito',
+          answers:["Obito","Madara","No-one","Danzo"],
       }
       ,
-      { question:'What popular Mexican dish literally translates as ‘little donkey’?',
-          rightAnswer:'Burrito',
-          answers:["Burrito","Enchilladas","Frijoles","Taco"],
+      { question:'Which Akatsuki member launched an attack on the Hidden Leaf Village that led to the death of the 3rd Hokage?',
+          rightAnswer:'Orochimaru',
+          answers:["Orochimaru","Pain","Itachi","Kissame"],
       }
       ,
-      { question:'How many herbs and spices make up the KFC spice blend?',
-          rightAnswer:'11',
-          answers:["7","9","11","15"],
+      { question:'What does the symbol on Gaaras forehead mean?',
+          rightAnswer:'Love',
+          answers:["Love","Pain","1Sealing","Three tails"],
       }
       ,
-      { question:'What is Japanese Sake made from?',
-          rightAnswer:'Rice',
-          answers:["Rice","Barley","Wheat","Wasabi"],
+      { question:'Name the fifth Hokage',
+          rightAnswer:'Tsunade',
+          answers:["Tsunade","Naruto","Itachi","Kakashi"],
       }
       
         ]
         const TV5=[
-          { question:'What is the main ingredient used to make hummus?',
-            rightAnswer:'Chickpeas',
-            answers:["Tahini","Humms","Chickpeas","Navy Beans"],
+          { question:'This famous tv show has yellow charaters',
+            rightAnswer:'Simpsons',
+            answers:["Simpsons","Family Guy","American Dad","Bobs Burgers"],
         }
         ,
-        { question:'Which famous soft drink was invented in 1892?',
-            rightAnswer:'Coca Cola',
-            answers:["Coca Cola","Pepsi","Surge","Fanta"],
+        { question:'What breed is the dog Snoopy from the popular TV show/cartoon, The Peanuts?',
+            rightAnswer:'Beagle',
+            answers:["Beagle","Greyhound","Corgi","Jack Russel Terrier"],
         }
         ,
-        { question:'Which is the only food that can never go off or go bad?',
-            rightAnswer:'Honey',
-            answers:["Honey","Twinkies","Peanuts","Canned Tuna"],
+        { question:'What show follows a city goverment department in Pawnee, Indiana',
+            rightAnswer:'Parks',
+            answers:["The Office","Parks and Rec","The West Wing","Sewage: A look behind the curtain"],
         }
         ,
-        { question:'What popular Mexican dish literally translates as ‘little donkey’?',
-            rightAnswer:'Burrito',
-            answers:["Burrito","Enchilladas","Frijoles","Taco"],
+        { question:'Who lives in a Pineapple under the sea?',
+            rightAnswer:'Spongebob Squarepants',
+            answers:["Spongebob Squarepants","Spongebob RoundShirt","Patrick Star","Mr. Krabs"],
         }
         ,
-        { question:'How many herbs and spices make up the KFC spice blend?',
-            rightAnswer:'11',
-            answers:["7","9","11","15"],
+        { question:'What city does the office take place in?',
+            rightAnswer:'Scranton',
+            answers:["Scranton","Erie","Dallas","Trenton"],
         }
         ,
-        { question:'What is Japanese Sake made from?',
-            rightAnswer:'Rice',
-            answers:["Rice","Barley","Wheat","Wasabi"],
+        { question:'Where finction city does Larulie and Rory live in',
+            rightAnswer:'Stars Hollow',
+            answers:["Sleepy Hollow","Stars Hollow","Breakingride","Bridgeton"],
         }
         
           ]
